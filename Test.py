d6f21f18c3fa722b74ecf3456ce114402fd739fe
dschreck17 <david.schreck@optonline.net>
2015-03-19 15:08:36 -0400

    Display error message if login credential fields are empty

--- Test.py
+++ Test.py
@@ -4,15 +4,18 @@ import tkinter
 from ftplib import FTP
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 def login(*args):
-    try:
+    if len(server.get()) != 0 and len(username.get()) != 0 and len(password.get()) != 0:
         ftp = FTP(server.get())
         ftp.login(username.get(), password.get())
         print(ftp.getwelcome())
-    except ValueError:
-        pass
+    else:
+        messagebox.showerror("Login Failed", "Incorrect login credentials.", )
 
+
+#GUI CODE
 root = Tk()
 root.title("Login To Server")
 
@@ -21,10 +24,12 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
+# Entry Field String Variables
 server = StringVar()
 username = StringVar()
 password = StringVar()
 
+# Sets up the input fields
 server_entry = ttk.Entry(mainframe, width=20, textvariable=server)
 server_entry.grid(column=2, row=1, sticky=(W, E))
 
@@ -34,15 +39,19 @@ username_entry.grid(column=2, row=2, sticky=(W, E))
 password_entry = ttk.Entry(mainframe, width=20, textvariable=password)
 password_entry.grid(column=2, row=3, sticky=(W, E))
 
+# Sets up the Labels
 ttk.Label(mainframe, text="Server: ").grid(column=1, row=1, sticky=W)
 ttk.Label(mainframe, text="Username: ").grid(column=1, row=2, sticky=W)
 ttk.Label(mainframe, text="Password: ").grid(column=1, row=3, sticky=W)
 
+# Sets up the Login Button
 ttk.Button(mainframe, text="Login", command=login).grid(column=2, row=4, sticky=(W, E))
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
+# Gives server_entry the blinking cursor
 server_entry.focus()
+# Binds the "Enter" key to the login method
 root.bind('<Return>', login)
 
 root.mainloop()
